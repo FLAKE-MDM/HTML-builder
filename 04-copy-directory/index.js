3ea285b62aef5ab378947c65652bb125720672f3
FLAKE_MDM <flake333@yandex.ru>
2023-05-08 02:38:13 +0300

    feat: add copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,54 @@
+const path = require('path');
+const fs =  require('fs');
+const { copyFile, constants } =  require('fs').promises;
 
+const srcFolder = path.join(__dirname, 'files');
+const destFolder = path.join(__dirname, 'files-copy');
+
+async function copyFolder(){
+  const createDir = await fs.promises.mkdir(destFolder, { recursive: true }, error => {
+    if(error){
+      console.log("Error:", error.message)
+    }
+  });
+
+  fs.readdir(destFolder, { withFileTypes: true }, (error, files) => {
+    if (error) {
+      console.log("Error:", error.message);
+      return;
+    }
+
+    files.forEach(file => {
+      if(file.isFile()){
+        fs.unlink(path.join(destFolder, file.name), error => {
+          if (error) {
+            console.error("Error:", error.message);
+          }
+        })
+      }
+    });
+  });
+
+  fs.readdir(srcFolder, { withFileTypes: true }, (error, files) => {
+    if (error) {
+      console.log("Error:", error.message);
+      return;
+    }
+
+    files.forEach(file => {
+      if(file.isFile()){
+        copyItem(file.name)
+      }
+    });
+  });
+}
+
+async function copyItem(fileName){
+  try {
+    await copyFile(path.join(srcFolder, fileName), path.join(destFolder, fileName));
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+copyFolder();
